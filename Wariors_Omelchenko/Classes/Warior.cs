eff660453652c296ceeed3deaaf9aff0b3411be7
danya_omelchenko <danilka.d.2006@gmail.com>
2024-12-03 22:14:10 +0500

    Добавлен класс Warior Omelchenko

---
+++ Wariors_Omelchenko/Classes/Warior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wariors_Omelchenko.Classes
+{
+    public class Warior
+    {
+        public double Health { get; set; }
+        public double Damage { get; set; }
+        public Warior(double Health, double Damage)
+        {
+            this.Health = Health;
+            this.Damage = Damage;
+        }
+    }
+}
