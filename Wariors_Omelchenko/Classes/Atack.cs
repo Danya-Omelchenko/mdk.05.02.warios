8cf58d5704477d7b5afe1ba22ad5a58adb8b0a06
danya_omelchenko <danilka.d.2006@gmail.com>
2024-12-03 22:19:44 +0500

    Add class Atack Omelchenko

---
+++ Wariors_Omelchenko/Classes/Atack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wariors_Omelchenko.Classes
+{
+    public class Atack : Warior
+    {
+        public Atack(double Health, double Damage) : base(Health, Damage)
+        {
+        }
+        public override void HaveDamage(double damage)
+        {
+            this.Health -= damage * this.Damage;
+        }
+    }
+}
--- Wariors_Omelchenko/Classes/Warior.cs
+++ Wariors_Omelchenko/Classes/Warior.cs
@@ -10,6 +10,10 @@ namespace Wariors_Omelchenko.Classes
     {
         public double Health { get; set; }
         public double Damage { get; set; }
+        public virtual void HaveDamage(double damage)
+        {
+
+        }
         public Warior(double Health, double Damage)
         {
             this.Health = Health;
