6f1bc7c6596e3228b5b1a54cadbb902d16788641
Данил О <Danil506@outlook.com>
2024-12-04 14:20:49 +0500

    Добавлен код для MainWindow Omelchenko

--- Wariors_Omelchenko/MainWindow.xaml.cs
+++ Wariors_Omelchenko/MainWindow.xaml.cs
@@ -10,8 +10,10 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wariors_Omelchenko.Classes;
 
 namespace Wariors_Omelchenko
 {
@@ -20,9 +22,26 @@ namespace Wariors_Omelchenko
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Atack warior = new Atack(100, 0.75);
+        public HavyAtack superWarior = new HavyAtack(100, 0.5);
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public void updateData()
+        {
+            enemyHealth.Content = "Жизненые показатели: " + Convert.ToString(superWarior.Health);
+            playerHealth.Content = "Жизненые показатели: " + Convert.ToString(warior.Health);
+        }
+        private void Attack(object sender, MouseButtonEventArgs e)
+        {
+            warior.HaveDamage(10);
+            updateData();
+        }
+        private void AttackHavy(object sender, MouseButtonEventArgs e)
+        {
+            superWarior.HaveDamage(10);
+            updateData();
         }
     }
 }
